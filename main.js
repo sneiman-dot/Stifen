d221e9137b6b5a70e6bcf68f05c1722b1bd6c410
sneiman-dot <hanssneiman@gmail.com>
2026-02-01 22:40:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ function openLetter() {
     content.classList.remove('hidden');
   }, 800);
 }
+
+function openLetterCvz() {
+  const cvz = document.getElementById('cvz');
+  const contentcvz = document.getElementById('contentcvz');
+
+  cvz.classList.add('fade-out');
+
+  setTimeout(() => {
+    cvz.style.display = 'none';
+    contentcvz.classList.remove('hidden');
+  }, 800);
+}
+
